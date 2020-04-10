61143e76652cae4ccfd38b6607bb2686c5f47adc
Raphael <rchalme_calvet@zoho.com>
2020-04-10 23:40:00 +0200

    add patch_of_size 256 for image above 1000p

--- runway_model.py
+++ runway_model.py
@@ -20,7 +20,7 @@ def setup(opts):
 def upscale(rdn, inputs):
     width, height = inputs['image'].size
     if width >= 1000 or height >= 1000:
-        return rdn.predict(np.array(inputs['image']), batch_size=10, by_patch_of_size=40, padding_size=5)
+        return rdn.predict(np.array(inputs['image']), batch_size=10, by_patch_of_size=256, padding_size=5)
     else:
         return rdn.predict(np.array(inputs['image']), batch_size=1)
 
