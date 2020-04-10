14046f88dfba3fa9aebb7454ad71ba5d79fb9b96
Raphael <rchalme_calvet@zoho.com>
2020-04-10 19:27:28 +0200

    correct rddn bug

--- runway_model.py
+++ runway_model.py
@@ -7,7 +7,7 @@ from ISR.models import RDN, RRDN
 def setup(opts):
     print("Checkpoints: ", opts["checkpoint"])
     if "C4" in opts['checkpoint']:
-        rdn  = RRDN(arch_params={'C':4, 'D':3, 'G':64, 'G0':64, 'T':10})
+        rdn  = RRDN(arch_params={'C':4, 'D':3, 'G':64, 'G0':64, 'T':10, 'x':4})
     elif "C6" in opts["checkpoint"]:
         rdn = RDN(arch_params={'C':6, 'D':20, 'G':64, 'G0':64, 'x':2})
     else:
